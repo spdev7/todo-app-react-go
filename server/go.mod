a676749f94dc97e937b9d572fe3a493474460923
Saurav Prakash <sauravprakash@spdev7.local>
2024-01-03 11:53:43 +0530

    Initial setup

---
+++ server/go.mod
@@ -0,0 +1,22 @@
+module github.com/spdev7/todo-app-react-go
+
+go 1.21.5
+
+require (
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/gofiber/fiber/v2 v2.51.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/yuin/goldmark v1.6.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/tools v0.16.1 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	// "github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
+
+func main() {
+	fmt.Print("This is the starting point of server")
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	todos := []Todo{}
+
+	app.Post("/api/v1/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+	})
+
+	app.Patch("/api/v1/todo/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+		return c.JSON(todos)
+	})
+	app.Get("api/v1/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
